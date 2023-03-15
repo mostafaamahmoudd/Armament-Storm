6bf64b283282f505038a25a8edc70db832ae12bc
Ahmed Alaa Abdelrahman <105639245+AhmedAlaa19@users.noreply.github.com>
2023-03-15 20:43:18 +0200

    ....

---
+++ Armament Storm/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rB;
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+    }
+
+}
