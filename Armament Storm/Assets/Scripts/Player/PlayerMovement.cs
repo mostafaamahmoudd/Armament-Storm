cab58bfae51986b980173c2c37e030018f471b24
Ahmed Alaa Abdelrahman <105639245+AhmedAlaa19@users.noreply.github.com>
2023-03-15 21:03:51 +0200

    ...

--- Armament Storm/Assets/Scripts/Player/PlayerMovement.cs
+++ Armament Storm/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,18 +2,48 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Animator anim;
+
+
+
     public float moveSpeed;
     public Rigidbody2D rB;
-
+    private Vector2 moveDirection;
+    private void Update()
+    {
+        ProcessInputs();
+    }
     void FixedUpdate()
     {
-        
+        Move();
     }
 
     void ProcessInputs()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+        if(moveDirection.magnitude > 0.002)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+        
+       
+            
+          
+        
     }
+
+    void Move()
+    {
+        rB.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        
+    } 
 
 }
