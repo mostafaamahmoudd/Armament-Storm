8a98fb291236ab60020e26f46b3b00e588528c78
Ahmed Alaa Abdelrahman <105639245+AhmedAlaa19@users.noreply.github.com>
2023-03-22 18:00:51 +0200

    and player gfx

--- Armament Storm/Assets/Scripts/Player/PlayerAttack.cs
+++ Armament Storm/Assets/Scripts/Player/PlayerAttack.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    public SpriteRenderer sprite;
     public Animator anim;
+    public CircleCollider2D circle;
+    private void Start()
+    {
+        circle.enabled = false;
+    }
     // Update is called once per frame
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+
+
+        }
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             anim.SetBool("IsAttacking", true);
+            circle.enabled = true;
+           
         }
         else
         {
             anim.SetBool("IsAttacking", false);
+            circle.enabled = false;
         }
            
 
